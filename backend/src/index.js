983c2e18c71dccfb0f4c4d940a87f5e569370d54
mraza176 <mrazaraheel12@gmail.com>
2025-03-25 00:53:00 +0500

    added message controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
