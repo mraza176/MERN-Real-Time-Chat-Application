a1c828480ad1b08cc21b202db2ac2c6e16a7822b
mraza176 <mrazaraheel12@gmail.com>
2025-03-25 00:21:50 +0500

    completed auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -111,3 +111,11 @@ export const updateProfile = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
